001b3d9cb01863588f965c3102c1defee588f1fb
R <90172092+r-ramos2@users.noreply.github.com>
2025-09-24 17:20:55 -0600

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,6 +4,7 @@ variable "region" {
   type        = string
   default     = "us-east-1"
 }
+
 variable "key_name_prefix" {
   description = "Prefix for the auto-generated SSH keypair"
   type        = string
@@ -16,39 +17,43 @@ variable "vpc_cidr_block" {
   type        = string
   default     = "10.0.0.0/16"
 }
+
 variable "public_subnet_cidr" {
   description = "CIDR block for the public subnet"
   type        = string
   default     = "10.0.1.0/24"
 }
+
 variable "availability_zone" {
   description = "AZ for the public subnet"
   type        = string
   default     = "us-east-1a"
 }
+
 variable "my_ip" {
   description = "Your IP CIDR block for SSH access (override this)"
   type        = string
-  default     = "203.0.113.0/32" # your home IP
+  default     = "203.0.113.0/32"
 }
 
- # AMI Lookup
+# AMI Lookup
 variable "linux2_ami_owner" {
   description = "Owner ID for the Amazon Linux 2 AMI"
   type        = string
   default     = "amazon"
 }
- variable "linux2_ami_name_filter" {
-   description = "Filter to find the latest Amazon Linux 2 AMI"
-   type        = string
-   default     = "amzn2-ami-hvm-*-gp2"
- }
+
+variable "linux2_ami_name_filter" {
+  description = "Filter to find the latest Amazon Linux 2 AMI"
+  type        = string
+  default     = "amzn2-ami-hvm-*-gp2"
+}
 
 # Security
 variable "allowed_cidr" {
-  description = "CIDR block permitted to reach application ports (HTTP/HTTPS/etc.)"
+  description = "CIDR block permitted to reach application ports"
   type        = string
-  default     = "203.0.113.25/32" # your home IP
+  default     = "203.0.113.25/32"
 }
 
 # Ports
@@ -57,26 +62,31 @@ variable "ssh_port" {
   type        = number
   default     = 22
 }
+
 variable "http_port" {
   description = "HTTP port for React App"
   type        = number
   default     = 80
 }
+
 variable "https_port" {
   description = "HTTPS port for secure access"
   type        = number
   default     = 443
 }
+
 variable "jenkins_port" {
   description = "Jenkins Web UI port"
   type        = number
   default     = 8080
 }
+
 variable "sonarqube_port" {
   description = "SonarQube UI port"
   type        = number
   default     = 9000
 }
+
 variable "react_port" {
   description = "React App port"
   type        = number
