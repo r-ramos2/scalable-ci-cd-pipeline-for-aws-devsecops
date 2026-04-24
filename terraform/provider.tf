df53fd1750d702707651a69342eaca62f8417bcd
R <90172092+r-ramos2@users.noreply.github.com>
2026-04-24 02:31:57 -0600

    Update AWS provider version to ~> 5.0

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.5.0"
   required_providers {
-    aws    = { source = "hashicorp/aws", version = ">= 4.0" }
+    aws    = { source = "hashicorp/aws", version = "~> 5.0" }
     tls    = { source = "hashicorp/tls" }
     random = { source = "hashicorp/random" }
     local  = { source = "hashicorp/local" }
