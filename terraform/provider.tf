1117507abd2fc670e2ba683770129de91eed582a
R <90172092+r-ramos2@users.noreply.github.com>
2026-05-17 11:46:41 -0600

    Remove local provider from provider.tf
    Removed local provider from required providers.

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -17,7 +17,6 @@ terraform {
     aws    = { source = "hashicorp/aws", version = "~> 5.0" }
     tls    = { source = "hashicorp/tls", version = "~> 4.0" }
     random = { source = "hashicorp/random", version = "~> 3.0" }
-    local  = { source = "hashicorp/local", version = "~> 2.0" }
   }
 }
 
